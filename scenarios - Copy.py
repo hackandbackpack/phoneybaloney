9f79b3e480645cd81ca86109c1ec38b60045c815
hackandbackpack <96545348+hackandbackpack@users.noreply.github.com>
2024-03-16 22:37:59 -0400

    Add files via upload

---
+++ scenarios - Copy.py
@@ -0,0 +1,65 @@
+
+def get_scenarios():
+    scenarios = [
+        {
+            'target_name': '0',
+              'description': 'Details about Extension 0',
+            'botdirections': 'Bot Directions for Extention 0',
+
+        },
+        {
+            'target_name': '10',
+              'description': 'Description for Extension 10.',
+            'botdirections': 'You are a Human Resources employee named Michael DuVall. You will use every opportunity you can to tell callers that you like Skittles and hate Peanut Butter.',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+        {
+            'target_name': '555',
+              'description': 'Description',
+            'botdirections': 'Directions',
+
+        },
+    ]
+    return scenarios    
